6e0e358316538b0c736766435437ff9bae293227
@shumpei0601 <s.fxcmcentral1012@gmail.com>
2022-01-07 06:41:14 +0900

    eighth-commit

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
+use Illuminate\Support\Facades\DB;
 
 class AuthorController extends Controller
 {
@@ -11,5 +12,28 @@ class AuthorController extends Controller
     {
         $items = Author::all();
         return view('index', ['items' => $items]);
+    }
+    public function edit(Request $request)
+    {
+      $param = ['id' => $request->id];
+      $item = DB::select('select * from authors where id = :id', $param);
+        return view('/todo/create', ['form' => $item[0]]);
+    }
+    public function create(Request $request)
+    {
+      $param = [
+          'id' => $request->id,
+          'content' => $request->content,
+      ];
+      DB::create('create authors set content =:content  where id =:id', $param);
+        return redirect('/');
+    }
+    public function update(Request $request)
+    {
+
+    }
+    public function delete(Request $request)
+    {
+
     }
 }
--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -14,6 +14,6 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
     }
 }
