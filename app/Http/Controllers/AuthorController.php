20b116c543f58064242b1b02e4c01b9c3d11c2ad
@shumpei0601 <s.fxcmcentral1012@gmail.com>
2022-01-08 06:28:48 +0900

    eighth-commit

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,30 +10,21 @@ class AuthorController extends Controller
 {
     public function index()
     {
-        $items = Author::all();
-        return view('index', ['items' => $items]);
+       $items = DB::table('authors')->get();
+       return view('index', ['items' => $items]);
     }
-    public function edit(Request $request)
+    
+    public function add()
     {
-      $param = ['id' => $request->id];
-      $item = DB::select('select * from authors where id = :id', $param);
-        return view('/todo/create', ['form' => $item[0]]);
+      return view('create');
+      //ここ微妙
     }
     public function create(Request $request)
     {
-      $param = [
-          'id' => $request->id,
-          'content' => $request->content,
+      $param= [
+        'content' => $request->content,
       ];
-      DB::create('create authors set content =:content  where id =:id', $param);
-        return redirect('/');
-    }
-    public function update(Request $request)
-    {
-
-    }
-    public function delete(Request $request)
-    {
-
+      DB::table('authors')->insert($param);
+      return redirect('/');
     }
 }
